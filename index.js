3b3d812182e8e9a502cb1e0cc87daa37fa83f21e
Nathan Hoad <nathan@nathanhoad.net>
2013-07-17 15:05:39 +1000

    Removed dependency on having a platform specific build tool

--- index.js
+++ index.js
@@ -2,74 +2,49 @@
 
 var program = require('commander'),
     fs = require('fs'),
-    path = require('path'),
-    packageJson = require('./package.json'),
-    watchPath = './views',
-    outputPath = './views',
+    path = require('path');
+
+var packageJson = require('./package.json'),
+    gvc = require('./gvc'),
+    watchPath = path.join(process.cwd(), 'pages'),
+    outputPath = path.join(process.cwd(), 'pages'),
     mainScriptFile = 'scripts/main.browser.js';
     
 program
   .version(packageJson.version)
   .option('-v, --verbose', 'Verbose output')
-  .option('-w, --watch [path]', 'Watch Path [default' + watchPath + ']')
-  .option('-o, --output [path]', 'Output Path [default' + outputPath + ']')
-  .option('-m, --mainScript [file]', 'Main Script File [default' + mainScriptFile + ']')
+  .option('-w, --watch [path]', 'Watch Path [default' + watchPath + ']', watchPath)
+  .option('-o, --output [path]', 'Output Path [default' + outputPath + ']', outputPath)
+  .option('-m, --mainScript [file]', 'Main Script File [default' + mainScriptFile + ']', mainScriptFile)
   .parse(process.argv);
 
-function hasError(error){
-    if(error){
+
+function hasError (error) {
+    if (error) {
         console.log(error.stack);
         return true;
     }
 }
 
-function log(message){
-    if(program.verbose){
+function log (message) {
+    if (program.verbose) {
         console.log(message);
     }
 }
 
-function parseView(file, callback){
-    var exec = require('child_process').exec,
-        command = path.join(path.dirname(process.argv[1]), 'build', 'output');
 
-    if(process.platform === 'win32') {
-        command = path.join(command, 'gvc.exe ')  + path.join(process.cwd(), mainScriptFile) + ' ' + file;
-    } else {
-        // TODO: add other OS calls
-    }
+log('Watching ' + watchPath + ' for changes. Output path is ' + outputPath);
 
-    exec(command, function (error, stdout, stderr) {
-        callback(error, stdout);
-    });
-}
-
-
-if(program.watch){
-    watchPath = program.watch;
-}
-
-if(program.output){
-    outputPath = program.output;
-}
-
-if(program.mainScript){
-    mainScriptFile = program.mainScript;
-}
-
-log('Watching ' + watchPath + ' for changes.');
-log('Output path is ' + outputPath);
-
-fs.watch(watchPath, function(eventType, filename){
-    if(eventType !== 'change' || path.extname(filename).toLowerCase() !== '.js'){
+fs.watch(watchPath, function (eventType, filename) {
+    if (eventType !== 'change' || path.extname(filename).toLowerCase() !== '.js') {
         return;
     }
-        
-    log('Processing: ' + filename);
     
-    parseView(path.resolve(path.join(watchPath, filename)), function(error, result){
-        if(!hasError(error)){
-            log(result);
+    log(filename + ' has changed.');
+    
+    gvc(path.resolve(path.join(watchPath, filename)), outputPath, function (error, result) {
+        if ( ! hasError(error)) {
+            log('Done');
         }
     });
 });
