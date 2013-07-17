612aa73fbf3eec00ba597cc1db6d7435589f3a41
Nathan Hoad <nathan@nathanhoad.net>
2013-07-17 17:05:15 +1000

    Tidied up

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ var packageJson = require('./package.json'),
     watchPath = path.join(process.cwd(), 'pages'),
     outputPath = path.join(process.cwd(), 'pages'),
     mainScriptFile = 'scripts/main.browser.js';
-    
+
+
 program
   .version(packageJson.version)
   .option('-v, --verbose', 'Verbose output')
@@ -19,13 +20,6 @@ program
   .parse(process.argv);
 
 
-function hasError (error) {
-    if (error) {
-        console.log(error.stack);
-        return true;
-    }
-}
-
 function log (message) {
     if (program.verbose) {
         console.log(message);
@@ -34,17 +28,11 @@ function log (message) {
 
 
 log('Watching ' + watchPath + ' for changes.');
-
 fs.watch(watchPath, function (eventType, filename) {
-    if (eventType !== 'change' || path.extname(filename).toLowerCase() !== '.js') {
-        return;
-    }
+    if (eventType !== 'change' || path.extname(filename).toLowerCase() !== '.js') return;
     
-    log(filename + ' has changed.');
-    
+    log(filename + ' has changed. Recompiling.');
     gvc(path.resolve(path.join(watchPath, filename)), outputPath, function (error, result) {
-        if ( ! hasError(error)) {
-            log('Done');
-        }
+        if (error) return console.log(error.stack);
     });
 });
