d315efc3b89e532ee3099cf7378fe73807559113
Nathan Hoad <nathan@nathanhoad.net>
2013-07-19 11:10:07 +1000

    Added some nicer logging

--- gvc.js
+++ gvc.js
@@ -1,3 +1,4 @@
+// Some views require a global window object to be present
 GLOBAL.window = {
     location: {
         pathname: ''
@@ -32,16 +33,18 @@ var fs = require('fs'),
     browserify = require('browserify');
 
 
-function parseDefinition (filename, outputPath, callback) {
+function parse (filename, outputPath, callback) {
     var object = browserify(filename).transform('gel-transform');
     
     object.bundle({}, function (error, data) {
         if (error) return callback(error);
         
         var json = JSON.stringify(eval(data)(1));
+        
+        // Cache buster
         json = json.replace('{', 
             '{"_" : "<style>*{display:none;}</style><script>window.location = window.location + \'?_bust=\' + +new Date();<\/script>", ');
-            
+        
         outputFilename = path.join(outputPath, path.basename(filename, path.extname(filename)) + '.json');
         
         fs.writeFile(outputFilename, json, function (error) {
@@ -52,4 +55,6 @@ function parseDefinition (filename, outputPath, callback) {
     });
 }
 
-module.exports = parseDefinition;
+module.exports = {
+    parse: parse
+};
